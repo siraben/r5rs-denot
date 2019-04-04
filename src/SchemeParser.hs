90577354cc15ae35f9f39152eb165231040c6d02
siraben <bensiraphob@hotmail.co.uk>
2019-04-04 18:58:41 +0700

    Overhaul parser, 367 -> 206 SLOC using parsec instead of hand-rolled

--- src/SchemeParser.hs
+++ src/SchemeParser.hs
@@ -2,195 +2,61 @@
 Module      : SchemeParser
 Description : Scheme parser according to R5RS specs.
 
-This is an implementation of combinator parsing from scratch,
-including various instance declarations for the 'Functor', 'Monad',
-'Applicative', 'MonadZero' and 'MonadPlus' typeclasses.
+Uses the ParserCombinator Haskell library.
+
 -}
 {-# LANGUAGE LambdaCase #-}
 
 module SchemeParser where
-
 import           Data.Char
+import           SchemeEval
 import           SchemeTypes
+import           Text.ParserCombinators.Parsec hiding (space)
 
-newtype Parser a =
-  Parser (String -> [(a, String)])
+a <||> b = try a <|> b
 
--- |Consume a single character.
-item :: Parser Char
-item =
-  Parser $ \case
-    "" -> []
-    (c:cs) -> [(c, cs)]
+-- |Transforms a parser into one that also consumes trailing whitespace.
+tok p = do
+  x <- p
+  space
+  return x
 
-parse (Parser p) = p
+eol = char '\n'
 
-instance Functor Parser where
-  fmap f (Parser cs) = Parser (\s -> [(f a, b) | (a, b) <- cs s])
+schemeComment = char ';' >> many (noneOf "\n") >>  eol
 
-instance Applicative Parser where
-  pure = return
-  (Parser cs1) <*> (Parser cs2) =
-    Parser (\s -> [(f a, s2) | (f, s1) <- cs1 s, (a, s2) <- cs2 s1])
-
--- |A monad with a zero (i.e. "failure").
-class Monad m =>
-      MonadZero m
-  where
-  zero :: m a
-
-instance Monad Parser where
-  return a = Parser $ \cs -> [(a, cs)]
-  p >>= f = Parser $ \cs -> concat [parse (f a) cs' | (a, cs') <- parse p cs]
-
-class MonadZero m =>
-      MonadPlus m
-  where
-  (<|>) :: m a -> m a -> m a
-
-instance MonadZero Parser where
-  zero = Parser (const [])
-
-instance MonadPlus Parser where
-  p <|> q = Parser (\cs -> parse p cs ++ parse q cs)
-
--- |Given two parsers @p@ and @q@, create a new parser such @q@ is
--- tried if @p@ failed.
-(+++) :: Parser a -> Parser a -> Parser a
-p +++ q =
-  Parser
-    (\cs ->
-       case parse (p <|> q) cs of
-         []     -> []
-         (x:xs) -> [x])
-
--- |Lift a predicate into a parser.
-sat :: (Char -> Bool) -> Parser Char
-sat p = do
-  c <- item
-  if p c
-    then return c
-    else zero
-
--- |Consume a specific character.
-char :: Char -> Parser Char
-char c = sat (c ==)
-
--- |Consume a specific string.
-string :: String -> Parser String
-string "" = return ""
-string (c:cs) = do
-  char c
-  string cs
-  return (c : cs)
-
--- |Turns a parser into its Kleene star variant.
-many :: Parser a -> Parser [a]
-many p = many1 p +++ return []
-
--- |Turns a parser into its Kleene plus variant.
-many1 :: Parser a -> Parser [a]
-many1 p = do
-  a <- p
-  as <- many p
-  return (a : as)
-
--- |Given two parsers @a@, @b@, return a parser @c@ such that it
--- accepts the language given by @a@ separated by @b@, and drops the
--- results of the separator.
-sepby :: Parser a -> Parser b -> Parser [a]
-p `sepby` sep = (p `sepby1` sep) +++ return []
-
--- |Like 'sepby', but at least one non-separator element must be
--- parsed.
-sepby1 :: Parser a -> Parser b -> Parser [a]
-p `sepby1` sep = do
-  a <- p
-  as <-
-    many $ do
-      sep
-      p
-  return (a : as)
-
-chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
-chainl p op a = (p `chainl1` op) +++ return a
-
-chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
-p `chainl1` op = do
-  a <- p
-  rest a
-  where
-    rest a =
-      (do f <- op
-          b <- p
-          rest (f a b)) +++
-      return a
-
--- |Parse a numeric digit.
-digit :: Parser Char
-digit = sat isDigit
-
-schemeComment = do
-  char ';'
-  many (sat  (/= '\n'))
-  return ' '
-
-schemeWhitespace = char ' ' <|> char '\n' <|> char '\t' <|> schemeComment
+schemeWhitespace = oneOf " \n\t" <|> schemeComment <?> "whitespace"
 
 -- |Parse whitespace.
-space :: Parser String
-space = many schemeWhitespace
-
--- |Lifts a parser into a whitespace-prefixed accepting one.
-token :: Parser a -> Parser a
-token p = do
-  a <- p
-  space
-  return a
+space :: Parser ()
+space = skipMany schemeWhitespace
 
 -- |Parse a symbol, consuming leading whitespace.
 symb :: String -> Parser String
-symb = token . string
-
--- |Parse characters up to the given terminator.
-upTo :: Char -> Parser String
-upTo c = many $ sat (/= c)
+symb = tok . string
 
 -- |Parse a number.
 nat :: Parser Integer
-nat = do
-  xs <- many1 digit
-  return (read xs :: Integer)
+nat = read <$> many1 digit
 
 -- |Like 'nat' but consumes leading whitespace.
-natural :: Parser Integer
-natural = token nat
+natural = tok nat
 
 -- |Parse a negative number.
-negnat = do
-  string "-"
-  n <- natural
-  return $ -n
+negnat = string "-" >> (-) 0 <$> natural
 
 -- |Parse a Scheme number.
-schemeNum :: Parser Expr
-schemeNum = do
-  x <- natural <|> negnat
-  return $ Const (Number x)
-
--- |The list of reserved Scheme Booleans.
-boolExprs :: [(String, Expr)]
-boolExprs = [("#t", Const (Boolean True)), ("#f", Const (Boolean False))]
+schemeNum = Const . Number <$> negnat <||> natural
 
 -- |Apply a parser, then when it succeeds, return a constant.
-constLit k c = do
-  k
-  return c
+constLit k c = k >> return c
 
--- |Parse a built-in constant (as defined in 'constExprs').
-schemeBool :: Parser Expr
-schemeBool =
-  foldr (\(s, const) rest -> constLit (symb s) const <|> rest) zero boolExprs
+-- |Parse a boolean.
+schemeBool = do
+  char '#'
+  (string "t" >> return (Const (Boolean True))) <|>
+   (string "f" >> return (Const (Boolean False)))
+
 
 initp x = x `elem` concat [['a' .. 'z'], ['A' .. 'Z'], "!$%&*/:<=>?_~"]
 
@@ -201,56 +67,45 @@ specialSubseqp x = x `elem` "+-.@"
 subseqp c = initp c || digitp c || specialSubseqp c
 
 schemeId =
-  do x <- sat initp
-     xs <- many (sat subseqp)
-     return (x : xs)
+  do {
+     x <- satisfy initp;
+     xs <- many (satisfy subseqp);
+     return (x : xs) }
      <|> string "+"
      <|> string "-"
-     <|> string "..."
+     <|> try (string "...")
+     <?> "identifier"
 
 lparen = symb "("
 
-rparen = do
-  space
-  symb ")"
+rparen = symb ")"
 
-dot = do
-  space
-  char '.'
-  space
+dot = symb "."
 
 formals :: Parser ([Ide], Maybe Ide)
 formals =
-  do { x <- schemeId;
-     return ([], Just x) } <|> do {
+  do { x <- schemeId; return ([], Just x) } <|> do {
     lparen;
-    rparen;
-    return ([], Nothing)
-     } <|> do {
-    lparen;
-    names <- schemeId `sepby1` space;
-    dot;
-    rest <- schemeId;
-    rparen;
-    return (names, Just rest) } <|> do {
-    lparen;
-    names <- schemeId `sepby1` space;
-    rparen;
-    return (names, Nothing)
-}
+    noArgs <- optionMaybe rparen;
+    (case noArgs of
+      Just _ -> return ([], Nothing)
+      Nothing -> do {
+                   names <- sepEndBy1 schemeId space;
+                   rest <- optionMaybe (dot >> schemeId);
+                   rparen;
+                   return (names, rest) })}
 
 schemeLambda = do
   lparen
   symb "lambda"
-  fs <- formals
-  space
-  exprs <- schemeExpr `sepby1` space
+  fs <- tok formals
+  exprs <- schemeExpr `sepBy1` space
   rparen
-  let (cmds, expr) = unsnoc exprs
+  return $ let (cmds, expr) = unsnoc exprs
    in case fs of
-        (args, Nothing)   -> return $ Lambda args cmds expr
-        ([], Just name)   -> return $ LambdaVV name cmds expr
-        (args, Just rest) -> return $ LambdaV args rest cmds expr
+        (args, Nothing)   -> Lambda args cmds expr
+        ([], Just name)   -> LambdaVV name cmds expr
+        (args, Just rest) -> LambdaV args rest cmds expr
 
 unsnoc xs = loop [] xs
   where
@@ -267,101 +122,85 @@ reifyQuotedList [e]    = App (Id "cons") [e, Const Nil]
 reifyQuotedList (e:es) = App (Id "cons") [e,reifyQuotedList es]
 
 reifyImproperList :: [Expr] -> Expr
+reifyImproperList []     = error "cannot reify empty list"
 reifyImproperList [e, v] = App (Id "cons") [e, v]
 reifyImproperList (e:es) = App (Id "cons") [e,reifyImproperList es]
 
 schemeQuotedList = do {
   lparen;
-  exprs <- schemeQuotable `sepby1` space;
+  exprs <- schemeQuotable `sepBy1` space;
+  last <- optionMaybe (dot >> schemeQuotable);
   rparen;
-  return $ reifyQuotedList exprs }
-  <|> do {
-  lparen;
-  exprs <- schemeQuotable `sepby1` space;
-  dot;
-  last <- schemeQuotable;
-  rparen;
-  return $ reifyImproperList (exprs ++ [last])
+  return $ case last of
+      Nothing -> reifyQuotedList exprs
+      Just a  -> reifyImproperList (exprs ++ [a])
   }
 
 schemeQuotable =
-  schemeNum <|> schemeBool <|> schemeNil <|>
+  schemeNum <|> schemeBool <|> schemeNil <||>
   do { Const . Symbol <$> schemeId } <|> schemeQuotedList <|>
   do {
    x <- schemeQuoted;
    return $ App (Id "cons") [Const (Symbol "quote"), App (Id "cons") [x, Const Nil]]
 }
-   
 
+schemeQuoteSpecialForm = symb "'" >> schemeQuotable
+schemeQuoted = schemeQuoteSpecialForm
+  <|> do
+        lparen
+        symb "quote"
+        x <- schemeQuotable
+        rparen
+        return x
 
-schemeQuoted =
-  do char '\''
-     schemeQuotable
-     <|> do
-    lparen
-    string "quote"
-    space
-    x <- schemeQuotable
-    rparen
-    return x
-
-schemeNil = do
-  lparen
-  rparen
-  return $ Const Nil
+schemeNil = lparen >> rparen >> return (Const Nil)
 
 schemeApp = do
   lparen
-  exprs <- schemeExpr `sepby1` space
+  (e:es) <- schemeExpr `sepBy1` space
   rparen
-  return $ App (head exprs) (tail exprs)
+  return $ App e es
 
 schemeSet = do
   lparen
-  string "set!"
-  space
-  n <- schemeId
-  space
+  symb "set!"
+  n <- tok schemeId
   exp <- schemeExpr
   rparen
   return $ Set n exp
 
 schemeIf =
   do {
-    lparen;
-     string "if";
-     space;
-     p <- schemeExpr;
-     space;
-     c <- schemeExpr;
-     space;
-     a <- schemeExpr;
+     lparen;
+     symb "if";
+     p <- tok schemeExpr;
+     c <- tok schemeExpr;
+     a <- optionMaybe schemeExpr;
      rparen;
-     return $ If p c a
-    } <|> do
-    lparen
-    string "if"
-    space
-    p <- schemeExpr
-    space
-    c <- schemeExpr
-    rparen
-    return $ IfPartial p c
+     return $ case a of
+       Just a  ->  If p c a
+       Nothing -> IfPartial p c
+    }
 
-schemeExpr :: Parser Expr
-schemeExpr =
-  schemeLambda <|> schemeIf <|> schemeSet <|> schemeQuoted <|> schemeApp <|>
-  schemeNum <|>
-  schemeBool <|> do Id <$> schemeId
 
-parseExpr = do
-  space
-  schemeExpr
+schemeIdExpr = Id <$> schemeId
+
+schemeSpecialForm = schemeLambda <||> schemeIf <||> schemeSet <||> schemeApp
+
+schemeCompoundExpr = try schemeQuoted <|> schemeSpecialForm
+
+schemeExpr = schemeCompoundExpr <|> schemeNum <|>  schemeBool <|> schemeIdExpr
+
+
+parseExpr = space >> schemeExpr
 
 -- |Parse a string into a Scheme 'Expr', but return @Nothing@ if there
 -- was unconsumed input.
-readExpr :: String -> Maybe Expr
-readExpr s =
-  case parse parseExpr s of
-    (res, ""):_ -> Just res
-    _           -> Nothing
+readExpr :: String -> Either ParseError Expr
+readExpr = parse parseExpr ""
+
+-- |Parse and evaluate a string.
+reval :: String -> A
+reval s = case parse parseExpr "" s of
+  Right res -> evalStd res
+  Left err  -> ("Error: " ++ show err, Nothing, emptyStore)
--- src/SchemeRepl.hs
+++ src/SchemeRepl.hs
@@ -5,13 +5,6 @@ import SchemeTypes
 import           System.Exit
 import           System.IO
 
--- |Parse and evaluate a string.
-reval :: String -> A
-reval s = case parse parseExpr s of
-  (res, ""  ) : _ -> evalStd res
-  (_  , rest) : _ -> ("Unexpected characters: " ++ rest, Nothing, emptyStore)
-  []              -> ("Parsing failed" , Nothing, emptyStore)
-
 -- |Print the contents of a value of type 'Result' 'Val', or an error
 -- message.
 reportResult :: A -> IO ()
@@ -19,8 +12,7 @@ reportResult (a, Nothing, s) = putStrLn $ "Error: " ++ a
 reportResult (a, Just e, s) = let memusage = (length s - length builtInOps - 1)
                                   pluralize s x = s ++ if x > 1 || x == 0 then "s" else ""
   in
-  putStrLn $ concat
-                              [showFull (head e) s, a, "\nMemory used: ", show memusage, pluralize " cell" memusage]
+  putStrLn $ concat  [showFull (head e) s, a, "\nMemory used: ", show memusage, pluralize " cell" memusage]
 
 -- |The main REPL loop.
 repl :: IO ()
@@ -35,9 +27,19 @@ repl = do
     if exp == "" then repl else reportResult $ reval exp
     repl
 
--- |Read, evaluate, print a file.
+-- |Read, evaluate and print a file.
 repf :: String -> IO ()
 repf filename = do
   x <- openFile filename ReadMode
   y <- hGetContents x
   reportResult $ reval y
+
+-- |Read  and print a file.
+rpf :: String -> IO ()
+rpf filename = do
+  x <- openFile filename ReadMode
+  y <- hGetContents x
+  (case readExpr y of
+     Right a -> putStrLn $ show a
+     Left a -> putStrLn $ "Failed to parse file: " ++ show a
+     )
